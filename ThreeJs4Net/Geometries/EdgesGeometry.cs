5126ba3f6cd7973de6e037690e65f5a0fccdff82
Sachin Gupta <SachinG@leapthought.co.nz>
2021-03-15 15:11:45 -1000

    Finished converting EdgesGeometry and added a EdgesGeometryTests file to test the new feature.

--- ThreeJs4Net/Geometries/EdgesGeometry.cs
+++ ThreeJs4Net/Geometries/EdgesGeometry.cs
@@ -2,32 +2,35 @@
 using ThreeJs4Net.Core;
 using ThreeJs4Net.Math;
 
+
 namespace ThreeJs4Net.Geometries
 {
 	public class EdgesGeometry : BufferGeometry
 	{
-		//public int A;
-		//public int B;
-		//public int C;
-
 		/// <summary>
-		/// 
+		/// This can be used as a helper object to view the edges of a geometry.
 		/// </summary>
-		/// <param name="geometry"></param>
-		/// <param name="thresholdAngle"></param>
+		/// <param name="geometry"> Any geometry object. </param>
+		/// <param name="thresholdAngle">  An edge is only rendered if the angle (in degrees) between the face normals of the adjoining faces exceeds this value. default = 1 degree. </param>
 		public EdgesGeometry(BufferGeometry geometry, int thresholdAngle = 1)
 		{
+			this.type = "EdgesGeometry";
+
+			// buffer
+
 			List<float> vertices = new List<float>();
+
+			// helper variables
 
 			var thresholdDot = Mathf.Cos(MathUtils.DEG2RAD * thresholdAngle);
 			int[] edge = new int[2];
-			List<string> edges = new List<string>();
-			//int edge1;
-			//int edge2;
-
-			//int[] key, keys = { A, B, C };
-			string key = null;
+			IDictionary<string, Dictionary<string, int>> edges = new Dictionary<string, Dictionary<string, int>>();
+			int edge1;
+			int edge2;
+			string key;
 			string[] keys = { "A", "B", "C" };
+
+			// prepare source geometry
 
 			var geometry2 = new Geometry().FromBufferGeometry(geometry);
 
@@ -45,33 +48,38 @@ namespace ThreeJs4Net.Geometries
 
 				for (int j = 0; j < 3; j++)
 				{
-					var edge1 = (int)face.GetType().GetProperty(keys[j]).GetValue(face);
-					var edge2 = (int)face.GetType().GetProperty(keys[(j + 1) % 3]).GetValue(face);
+					edge1 = (int)face.GetType().GetField(keys[j]).GetValue(face);
+					edge2 = (int)face.GetType().GetField(keys[(j + 1) % 3]).GetValue(face);
 					edge[0] = Mathf.Min(edge1, edge2);
-					edge[1] = Mathf.Min(edge1, edge2);
+					edge[1] = Mathf.Max(edge1, edge2);
 
 					key = edge[0].ToString() + ',' + edge[1].ToString();
 
-					if (edges[key] == null)
+					if (!edges.ContainsKey(key))
 					{
-						edges[key] = { index1: edge[0], index2: edge[1], face1: i, face2: null };
+						var temp = new Dictionary<string, int> { { "index1", edge[0] }, { "index2", edge[1] }, { "face1", i }, { "face2", -1 } };	// ints cant be null
+						edges.Add(key, temp);
 					}
 
 					else
 					{
-						edges[key] = i;
+						edges[key]["face2"] = i;
 					}
 				}
 			}
 
-			foreach (var e in edges)
+			// generate vertices
+
+			var edgesValues = edges.Values;
+
+			foreach (var e in edgesValues)
 			{
-				if (e.face2 == null || faces[e.face1].Normal.Dot(faces[e.face2].Normal) <= thresholdAngle)
+				if (e["face2"] == -1 || faces[e["face1"]].Normal.Dot(faces[e["face2"]].Normal) <= thresholdDot)
 				{
-					var vertex = sourceVertices[e.index1];
+					var vertex = sourceVertices[e["index1"]];
 					vertices.Add(vertex.X, vertex.Y, vertex.Z);
 
-					vertex = sourceVertices[e.index2];
+					vertex = sourceVertices[e["index2"]];
 					vertices.Add(vertex.X, vertex.Y, vertex.Z);
 				}
 			}
